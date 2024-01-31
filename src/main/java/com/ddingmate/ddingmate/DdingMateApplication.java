e8000388534179d0ab00c4c7b4dee32c4c25f624
gyuchangShim <shim17@mju.ac.kr>
2024-01-31 13:49:26 +0900

    build: project 생성

---
+++ src/main/java/com/ddingmate/ddingmate/DdingMateApplication.java
@@ -0,0 +1,13 @@
+package com.ddingmate.ddingmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdingMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdingMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddingmate/ddingmate/DdingMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddingmate.ddingmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdingMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
