7d50fac6350a14fa1291c244292a858d29f31343
gyuchangShim <shim17@mju.ac.kr>
2024-02-16 16:54:58 +0900

    build: deploy.sh 설정 변경

--- deploy.sh
+++ deploy.sh
@@ -1,47 +1,47 @@
-##!/usr/bin/env bash
-#
-#REPOSITORY=/home/ubuntu/app
-#
-#echo "> 현재 구동 중인 애플리케이션 pid 확인"
-#
-#CURRENT_PID=$(pgrep -fla java | grep hayan | awk '{print $1}')
-#
-#echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
-#
-#if [ -z "$CURRENT_PID" ]; then
-#  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
-#else
-#  echo "> kill -15 $CURRENT_PID"
-#  kill -15 $CURRENT_PID
-#  sleep 5
-#fi
-#
-#echo "> 새 애플리케이션 배포"
-#
-#JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1)
-#
-#echo "> JAR NAME: $JAR_NAME"
-#
-#echo "> $JAR_NAME 에 실행권한 추가"
-#
-#chmod +x $JAR_NAME
-#
-#echo "> $JAR_NAME 실행"
-#
-#nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
-#!/bin/bash
-CURRENT_PID=$(pgrep -f .jar)
-echo "$CURRENT_PID"
-if [ -z $CURRENT_PID ]; then
-        echo "no process"
+#!/usr/bin/env bash
+
+REPOSITORY=/home/ubuntu/app
+
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -fla java | grep hayan | awk '{print $1}')
+
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-        echo "kill $CURRENT_PID"
-        kill -9 $CURRENT_PID
-        sleep 3
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
 fi
 
-JAR_PATH="/home/ubuntu/cicd/*.jar"
-echo "jar path : $JAR_PATH"
-chmod +x $JAR_PATH
-nohup java -jar $JAR_PATH >> /home/ubuntu/cicd/deploy.log 2>> /home/ubuntu/cicd/deploy_err.log &
-echo "jar fild deploy success"
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1)
+
+echo "> JAR NAME: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
+#!/bin/bash
+#CURRENT_PID=$(pgrep -f .jar)
+#echo "$CURRENT_PID"
+#if [ -z $CURRENT_PID ]; then
+#        echo "no process"
+#else
+#        echo "kill $CURRENT_PID"
+#        kill -9 $CURRENT_PID
+#        sleep 3
+#fi
+#
+#JAR_PATH="/home/ubuntu/cicd/*.jar"
+#echo "jar path : $JAR_PATH"
+#chmod +x $JAR_PATH
+#nohup java -jar $JAR_PATH >> /home/ubuntu/cicd/deploy.log 2>> /home/ubuntu/cicd/deploy_err.log &
+#echo "jar fild deploy success"
