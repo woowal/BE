1772db2520f9f735aa94bb4388bff11c21b36be2
gyuchangShim <shim17@mju.ac.kr>
2024-02-17 16:15:44 +0900

    feat: category state 추가

--- src/main/java/com/ddingmate/ddingmate/post/state/Category.java
+++ src/main/java/com/ddingmate/ddingmate/post/state/Category.java
@@ -2,8 +2,23 @@ package com.ddingmate.ddingmate.post.state;
 
 import lombok.Getter;
 
+import java.util.Arrays;
+
 @Getter
 public enum Category {
 
-    springboot, CATEGORY2
+    SPRINGBOOT("springboot"), REACT("react");
+
+    private final String value;
+
+    Category(String value) {
+        this.value = value;
+    }
+
+    public static Category fromCode(String db) {
+        return Arrays.stream(Category.values())
+                .filter(v -> v.getValue().equals(db))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(String.format("카테고리에 %s가 존재하지 않습니다.", db)));
+    }
 }
