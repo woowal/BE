34f5dd9610e56925f5150e958af352b06cb044c2
gyuchangShim <shim17@mju.ac.kr>
2024-02-17 17:07:41 +0900

    build: deploy.sh 설정 수정(삭제 시)

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
 REPOSITORY=/home/ubuntu/cicd
+PROJECT_NAME=dding-mate
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fla java | grep hayan | awk '{print $1}')
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*jar)
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
   echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -9 $CURRENT_PID"
-  kill -9 $CURRENT_PID
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
   sleep 5
 fi
 
