3c92d53df984f0a09a2ef895b2f4536c298ce9d4
gyuchangShim <shim17@mju.ac.kr>
2024-02-24 21:01:23 +0900

    refactor: commentService repository -> service로 변경

--- src/main/java/com/ddingmate/ddingmate/comment/service/CommentService.java
+++ src/main/java/com/ddingmate/ddingmate/comment/service/CommentService.java
@@ -7,8 +7,10 @@ import com.ddingmate.ddingmate.comment.dto.response.CommentResponse;
 import com.ddingmate.ddingmate.comment.repository.CommentRepository;
 import com.ddingmate.ddingmate.member.domain.Member;
 import com.ddingmate.ddingmate.member.repository.MemberRepository;
+import com.ddingmate.ddingmate.member.service.MemberService;
 import com.ddingmate.ddingmate.post.domain.Post;
 import com.ddingmate.ddingmate.post.repository.PostRepository;
+import com.ddingmate.ddingmate.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,20 +26,20 @@ import static com.ddingmate.ddingmate.util.exception.ExceptionEnum.NO_SUCH_COMME
 @RequiredArgsConstructor
 public class CommentService {
 
-    private final MemberRepository memberRepository;
-    private final PostRepository postRepository;
+    private final MemberService memberService;
+    private final PostService postService;
     private final CommentRepository commentRepository;
 
     @Transactional
     public void createComment(Long memberId, CreateCommentRequest createCommentRequest) {
-        Member member = memberRepository.getReferenceById(memberId);
-        Post post = postRepository.getReferenceById(createCommentRequest.getPostId());
+        Member member = memberService.retrieveMember(memberId);
+        Post post = postService.retrievePost(createCommentRequest.getPostId());
         commentRepository.save(createCommentRequest.toEntity(member, post));
     }
 
     @Transactional
     public void createReply(Long id, Long memberId, CreateReplyRequest createReplyRequest) {
-        Member member = memberRepository.getReferenceById(memberId);
+        Member member = memberService.retrieveMember(memberId);
         Comment parent = commentRepository.getReferenceById(id);
         Comment child = createReplyRequest.toEntity(member, parent);
         commentRepository.save(child);
